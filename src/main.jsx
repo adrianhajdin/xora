ab7e9ff030f2f0ec4a64f198d059ec49300fa3ef
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2024-12-05 14:32:36 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root')); 
+root.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>
+);
